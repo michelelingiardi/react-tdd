b7929153d87807996007f753a1f3ed9b90b139aa
Michele <michelelingiardi@gmail.com>
2020-03-24 18:58:26 -0300

    Teste - fechar modal

--- cypress/integration/adding_a_restaurant.spec.js
+++ cypress/integration/adding_a_restaurant.spec.js
@@ -4,9 +4,21 @@ describe('adding a restaurant', () => {
   it('displays the restaurant in the list', () => {
     cy.visit('http://localhost:1234');
 
+    // modal nao eh exibido
     cy.get('[data-test="newRestaurantName"]')
       .should('not.exist');
 
+    // modal pode ser cancelado
+    cy.get('[data-test="addRestaurantButton"]')
+      .click();
+
+    cy.get('[data-test="newRestaurantName"]')
+      .type("{esc}");
+
+    cy.get('[data-test="newRestaurantName"]')
+      .should('not.exist');
+
+    // adicionando novo restaurante
     cy.get('[data-test="addRestaurantButton"]')
       .click();
 
